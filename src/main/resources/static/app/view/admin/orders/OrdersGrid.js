5e79fc176ae1a07277cba354ee054510f38c54b5
Lev <consult.lev@gmail.com>
2016-12-18 10:21:48 +0300

    Внес изменения по виджету.

--- src/main/resources/static/app/view/admin/orders/OrdersGrid.js
+++ src/main/resources/static/app/view/admin/orders/OrdersGrid.js
@@ -1,101 +1,85 @@
-Ext.define('APP.view.admin.orders.OrdersGrid', {
-    extend : 'Ext.grid.Panel',
-    requires : [
-        'APP.store.dictionary.OrdersStore',
-        'APP.model.dictionary.Order',
-        'APP.model.dictionary.SubOrder',
-        'APP.view.admin.users.UsersGridController'
-    ],
+Ext.define('APP.view.admin.orders.OrdersGrid',
+		{
+			extend : 'Ext.grid.Panel',
+			requires : [ 'APP.store.dictionary.OrdersStore', 'APP.model.dictionary.Order', 'APP.model.dictionary.SubOrder',
+					'APP.view.admin.users.UsersGridController' ],
 
-    width : "*",
-    height : 400,
-    title : 'Заказы',
-    store : {
-        type : 'OrdersStore'
-    },
+			width : "*",
+			height : 400,
+			title : 'Заказы',
+			store : {
+				type : 'OrdersStore'
+			},
 
-    viewConfig : {
-        enableTextSelection : true
-    },
+			viewConfig : {
+				enableTextSelection : true
+			},
 
-    plugins: [
-        {
-            ptype: "gridfilters"
-        },
-        {
-            ptype: 'rowwidget',
-            widget: {
-                xtype: 'container',
-                layout: 'hbox',
-                bind: {
-                    foo: '{record}'
-                },
-                items: [
-                    {
-                        xtype: 'container',
-                        flex: 1
-                    },
-                    {
-                        xtype: 'grid',
-                        width: 500,
-                        bind: {
-                            store: '{record.subOrders}'
-                        },
-                        emptyText: 'Подзаказов нет',
-                        columns: [
-                            {
-                                xtype: 'rownumberer',
-                                text: '#'
-                            },
-                            {
-                                text: '№ Заказа',
-                                flex: 1,
-                                dataIndex: 'number'
-                            },
-                            {
-                                text: 'Статус',
-                                flex: 1,
-                                dataIndex: 'status'
-                            }
-                        ]
-                    }
-                ],
-                setFoo: Ext.emptyFn // костыль
-            }
-        }
-    ],
+			plugins : [ {
+				ptype : "gridfilters"
+			}, ],
 
-    columns: [
-        {
-            xtype: 'rownumberer',
-            text: '#'
-        },
-        {
-            text: '№ Заказа',
-            flex: 1,
-            dataIndex: 'number'
-        },
-        {
-            text: 'Статус',
-            flex: 1,
-            dataIndex: 'status'
-        },
-        {
-            text: 'Объект',
-            flex: 1,
-            dataIndex: 'object'
-        },
-        {
-            text: 'Исполнитель',
-            width: 500,
-            dataIndex: 'manager'
-        }
-    ],
+			columns : [ {
+				xtype : 'rownumberer',
+				text : '#'
+			}, {
+				text : '№ Заказа',
+				flex : 1,
+				dataIndex : 'number'
+			}, {
+				text : 'Статус',
+				flex : 1,
+				dataIndex : 'status'
+			}, {
+				text : 'Объект',
+				flex : 1,
+				dataIndex : 'object'
+			}, {
+				text : 'Исполнитель',
+				width : 500,
+				dataIndex : 'manager'
+			}, {
+				text : 'Button',
+				width : 400,
+				xtype : 'widgetcolumn',
+				widget : {
+					xtype : 'container',
+					layout : 'hbox',
+					bind : {
+						foo : '{record}'
+					},
+					items : [ {
+						xtype : 'container',
+						flex : 1
+					}, {
+						xtype : 'grid',
+						width : 500,
+						bind : {
+							store : '{record.subOrders}'
+						},
+						emptyText : 'Подзаказов нет',
+						columns : [ {
+							xtype : 'rownumberer',
+							text : '#'
+						}, {
+							text : '№ Заказа',
+							flex : 1,
+							dataIndex : 'number'
+						}, {
+							text : 'Статус',
+							flex : 1,
+							dataIndex : 'status'
+						} ]
+					} ],
+					setFoo : Ext.emptyFn
+				// костыль
+				}
+			} ],
 
-    listeners: {
-        select: function(grid, record) {
-            console.log(record);
-            console.log(record.subOrders());
-        }
-    }
-});
+			listeners : {
+				select : function(grid, record) {
+					console.log(record);
+					console.log(record.subOrders());
+				}
+			}
+		});
